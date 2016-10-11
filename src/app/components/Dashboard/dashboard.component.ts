610d5c1ebdbec411f87bea70820cbdba75c70578
Supachai <spluamjitta@dstworldwideservices.com>
2016-10-11 10:34:20 +0700

    - fixed br tag. - experimented grid system.

--- src/app/components/Dashboard/dashboard.component.ts
+++ src/app/components/Dashboard/dashboard.component.ts
@@ -6,8 +6,8 @@ import { NotifyService } from '../../services';
 @Component({
     template:  `
       dashboard detail :id
-      <a routerLink="/dashboard/detail/hello" routerLinkActive="active">Hello</a></br>
-      <a routerLink="/dashboard/detail/angular2" routerLinkActive="active">Angular2</a></br>
+      <a routerLink="/dashboard/detail/hello" routerLinkActive="active">Hello</a><br/>
+      <a routerLink="/dashboard/detail/angular2" routerLinkActive="active">Angular2</a><br/>
     `
 })
 
